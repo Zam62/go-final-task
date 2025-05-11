e044764042eae90496794f0db3850ec6eb00949a
Andrey <zamulaa006@gmail.com>
2025-05-11 22:48:07 +0300

    add grps server, handlers

--- internal/orchestrator/handlers.go
+++ internal/orchestrator/handlers.go
@@ -1,6 +1,7 @@
 package orchestrator
 
 import (
+	"context"
 	"encoding/json"
 	calculation "go-final-task/pkg"
 	"go-final-task/pkg/models"
@@ -8,7 +9,205 @@ import (
 	"net/http"
 	"strings"
 	"text/template"
+	"time"
 )
+
+func logsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Method: %s, URL: %s", r.Method, r.URL)
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(start)
+		log.Printf("Method: %s, completion time: %v", r.Method, duration)
+	})
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var token string
+		cookie, err := r.Cookie("jwt")
+		if checkCookie(cookie, err) {
+			token = cookie.Value
+			log.Print("token was taken from cookie")
+		} else {
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				errorResponse(w, "authorization is required", http.StatusUnauthorized)
+				log.Printf("Code: %v, user unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			tokenParts := strings.Split(authHeader, " ")
+			if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+				errorResponse(w, "invalid token format", http.StatusUnauthorized)
+				log.Printf("Code: %v, invalid token format", http.StatusUnauthorized)
+				return
+			}
+			token = tokenParts[1]
+			log.Print("token was taken from header")
+		}
+
+		claims, id := jwt.Verify(token)
+		if !claims {
+			errorResponse(w, "invalid token", http.StatusUnauthorized)
+			log.Printf("Code: %v, invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userID, id)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func databaseMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			errorResponse(w, "invalid request method", http.StatusMethodNotAllowed)
+			log.Printf("Code: %v, Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var body ExpressionReq
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			errorResponse(w, "internal server error", http.StatusInternalServerError)
+			log.Printf("Code: %v, error with decoding request body", http.StatusInternalServerError)
+			return
+		}
+
+		// добавляем выражение в базу данных и получаем id
+		log.Printf("Adding expression to database")
+		e := &models.Expression{
+			Expression: body.Expression,
+			Status:     "in process",
+			Result:     0.0,
+		}
+		expID, err := db.InsertExpression(r.Context(), e, r.Context().Value(userID).(int))
+		if err != nil {
+			errorResponse(w, "internal server error", http.StatusInternalServerError)
+			log.Printf("Code: %v, error with database", http.StatusInternalServerError)
+			return
+		}
+		respID := RespID{Id: int(expID)}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(respID)
+
+		// запускаем вычисления в фоновом режиме
+		go func() {
+			expr := &Expression{
+				exp: body.Expression,
+				id:  int(expID),
+			}
+			ctx := context.WithValue(r.Context(), ctxKey, expr)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}()
+	})
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errorResponse(w, "invalid request method", http.StatusMethodNotAllowed)
+		log.Printf("Code: %v, invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var body struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		errorResponse(w, "invalid request body", http.StatusBadRequest)
+		log.Printf("Code: %v, json decoding error", http.StatusBadRequest)
+		return
+	}
+
+	if len(body.Password) == 0 {
+		errorResponse(w, "password cannot be empty", http.StatusForbidden)
+		log.Printf("Code: %v, empty password", http.StatusForbidden)
+		return
+	}
+
+	pass, err := password.Generate(body.Password)
+	if err != nil {
+		errorResponse(w, "internal server error", http.StatusInternalServerError)
+		log.Printf("Code: %v, %s", http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx := r.Context()
+	user := &models.User{
+		Login:    body.Login,
+		Password: pass,
+	}
+	_, err = db.InsertUser(ctx, user)
+	if err != nil {
+		errorResponse(w, "user already exists", http.StatusConflict)
+		log.Printf("Code: %v, user %s already exists", http.StatusConflict, body.Login)
+		return
+	}
+
+	log.Printf("user: %v has successfully registered", user.Login)
+	w.WriteHeader(http.StatusOK)
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errorResponse(w, "invalid request method", http.StatusMethodNotAllowed)
+		log.Printf("Code: %v, invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var body struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		errorResponse(w, "invalid request body", http.StatusBadRequest)
+		log.Printf("Code: %v, json decoding error", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	user, err := db.SelectUserByLogin(ctx, body.Login)
+	if err != nil {
+		errorResponse(w, "user not fuond", http.StatusNotFound)
+		log.Printf("Code: %v, user %v was not found", http.StatusNotFound, body.Login)
+		return
+	}
+	if err := password.Compare(user.Password, body.Password); err != nil {
+		errorResponse(w, "incorrect password", http.StatusForbidden)
+		log.Printf("Code: %v, incorrect password", http.StatusForbidden)
+		return
+	}
+
+	var resp struct {
+		Jwt string `json:"jwt"`
+	}
+	token, err := jwt.Generate(int(user.ID))
+	if err != nil {
+		errorResponse(w, "internal server error", http.StatusInternalServerError)
+		log.Printf("Code: %v, error with generating token", http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(10 * time.Minute),
+		Path:     "/",
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	resp.Jwt = token
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
 
 type CalculationRequest struct {
 	Expression string `json:"expression"`
